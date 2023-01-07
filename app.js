dcfa7fee44ada52f9cbbb2a80e359efb039e7066
ladynazlia <amandalfwrites@gmail.com>
2023-01-06 22:01:54 -0500

    set up basic app with nonsense data

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
-
 const app = express();
+const data = require('./data.json');
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send(data);
 });
+
+app.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const toSend = data[id]
+  const validIds = data.length - 1;
+
+  if (toSend) {
+    res.send(data[id])
+  } else {
+    res.send(`Invalid ID, valid IDs: 0-${validIds}`);
+  }
+})
 
 app.listen(3000, () => {
   console.log('Express server listening on port 3000');
